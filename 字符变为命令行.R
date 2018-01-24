0b36a9e0529887422135eaf9730843547c8d87a0
goal1234 <946883098@qq.com>
2018-01-24 18:37:26 +0800

    -

--- 字符变为命令行.R
+++ 字符变为命令行.R
@@ -1,4 +1,20 @@
 x <- 1:10
 a <- "print(x)"
 class (a)
-eval(parse(text = a))
+eval(parse(text = 'mycar'))
+
+
+# 生成很多数据框
+s <- 'data <- 1:100'
+eval(parse(text=s))
+
+for(i in 1:100) {
+  df <- data.frame(x = rnorm(100))
+  
+  name <- paste('d',i, sep ='')
+  print(name)
+  
+  line <- paste(name,'<- df[sample(nrow(df), 100,replace = T),]',sep ='')
+  eval(parse(text = line))
+}
+
